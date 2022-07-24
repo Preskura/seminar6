35f97625bd13b04d84242e25423417581d93bee5
Алина Прескура <a.preskura@logic-way.local>
2022-07-24 20:27:58 +0300

    task41

---
+++ sem6/task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача41. Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+namespace Zadacha41
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             Console.WriteLine("Введите количество чисел, которое хотите ввести: ");
+             int size = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine();
+             int number = 0;
+             int result = 0;
+             while(number < size)
+             {
+               Console.Write("Введите число: ");
+               int i = Convert.ToInt32(Console.ReadLine());
+               if (i > 0)
+               {
+                  result++;
+                  number++;
+               }
+               else number++;
+               Console.WriteLine();
+             }
+             Console.WriteLine($"Количество чисел больше нуля: {result}");
+         }
+     }  
+}   
